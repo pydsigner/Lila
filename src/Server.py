75a22c5dbf03ff493fe997f3bd50081881d127df
Jacob <jkbbwr@gmail.com>
2012-03-11 23:44:19 +0000

    Committing more.

--- src/Server.py
+++ src/Server.py
@@ -4,41 +4,117 @@ from twisted.protocols.basic import LineReceiver
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
 from twisted.python import log
-import time
+#from database import redis_database as database
+from database import test_dabase as database
+import uuid
 import json
 import sys
 
 def shutdownOnError(message):
+    """Die hard on any exceptions in the program."""
     if message["isError"]:
         reactor.stop()
 
+
 class Server(LineReceiver):
+    def __init__(self):
+        self.state = "PRE_AUTH"
+        self.last_ping = None
+        self.alive = True
+
     def lineReceived(self, line):
+        """Handle a line that just came down the pipe, dispatch it depending on our state and the contents."""
+        try:
+            data = json.loads(line)
+        except Exception:
+            self.reject_kindly_with_msg("Unable to decode?")
+            return
         if self.state == "PRE_AUTH":
-            self.handle_auth(line)
+            self.handle_auth(data)
+        if data.get("PONG"):
+            self.handle_pong(data)
+        if data.get("MSG"):
+            self.handle_msg(data)
+        if data.get("DC"):
+            self.handle_dc(data)
+
+    def handle_pong(self, data):
+        """Handle a PING response. If its not a match then die hard and fast."""
+        if data.get("PONG") != self.last_ping.get("PING"):
+            self.transport.loseConnection()
         else:
-            self.handle_line(line)
+            self.alive = True
 
-    def handle_line(self, line):
-        pass
+    def handle_msg(self, data):
+        """
+        Handle a message, if its addressed to someone then post it to them,
+        otherwise tell my client that they are not online
+        """
+        target = self.factory.connections[data.get("TO", None)]
+        if target is None:
+            response = {"STATUS": "FAIL", "INFO": "Failed to find partner online."}
+            self.sendLine(json.dumps(response))
+        else:
+            target.sendLine(json.dumps(data))
 
-    def handle_auth(self, line):
-        pass
+    def handle_auth(self, data):
+        """
+        Authenticate a user. See if the user:password pair match what we have in the database.
+        If this is the case then tell them its good, update their state, otherwise reject them.
+        """
+        user = data.get("USER")
+        password = data.get("PASS")
+        if database.authenticate_user(user, password):
+            response = {"STATUS": "OK"}
+            self.sendLine(json.dumps(response))
+            self.user = user
+            self.factory.connections.pop(self.hostname)
+            self.factory.connections[self.user] = self
+            self.state = "LIVE"
+            log.msg("Authenticated user %s. All is well, connections dict updated." % self.hostname)
+        else:
+            log.msg("%s failed to authenticated, dropping their connection." % self.hostname)
+            self.reject_kindly()
 
     def heartbeat(self):
-        #noinspection PyTypeChecker
-        self.sendLine(time.asctime())
-        reactor.callLater(5, self.heartbeat)
+        if not self.alive:
+            self.reject_kindly_with_msg("Did not reply to last ping.")
+            return
+        log.msg("Sending heartbeat.")
+        self.last_ping = {"PING": uuid.uuid4().hex}
+        self.sendLine(json.dumps(self.last_ping))
+        self.alive = False # schrodinger's cat, lets assume the client is dead until they tell us otherwise
+        self.beat = reactor.callLater(25, self.heartbeat)
 
     def connectionLost(self, reason):
         self.factory.clients -= 1
-        self.factory.connections.pop(self.hostname)
+        try:
+            self.factory.connections.pop(self.hostname)
+        except KeyError:
+            # Could be they are stored under the username not their hostname.
+            self.factory.connections.pop(self.user)
 
     def reject_kindly(self):
-        answer = {"STATUS" : "CONNECTION_REJECTED"}
+        log.msg("Rejected client: %s" % self.hostname)
+        answer = {"STATUS": "REJECTED"}
         self.sendLine(json.dumps(answer))
         # Grace period for killing the connection.
-        reactor.callLater(3, self.transport.loseConnection)
+        reactor.callLater(1.5, self.transport.loseConnection)
+
+    def reject_kindly_with_msg(self, msg):
+        log.msg("Rejected client: %s for reason %s" % (self.user, msg))
+        answer = {"STATUS": "REJECTED", "INFO": msg}
+        self.sendLine(json.dumps(answer))
+        # Grace period for killing the connection.
+        reactor.callLater(1.5, self.transport.loseConnection)
+
+    def authed_in_time(self):
+        """
+        The user failed to send the right authentication inside of the 20 second time limit.
+        """
+        if self.state != "LIVE":
+            log.msg("Disconnecting user %s for failing to authenticate in-time" % self.hostname)
+            self.reject_kindly()
 
     def connectionMade(self):
         self.state = "PRE_AUTH"
@@ -49,9 +125,13 @@ class Server(LineReceiver):
             self.factory.clients += 1
         else:
             self.reject_kindly()
+        reactor.callLater(20, self.authed_in_time)
+        reactor.callLater(25, self.heartbeat)
+
 
 class ServerFactory(Factory):
     protocol = Server
+
     def __init__(self):
         self.connections = {}
         self.clients = 0
---
+++ src/database.py
@@ -0,0 +1,25 @@
+__author__ = 'Jakob'
+import redis
+
+class RedisDatabase(object):
+    def __init__(self, host, port):
+        self.db = redis.Redis(host, port, password="letmeinnow")
+        self.user_db = redis.Redis(host, port, db=1, password="letmeinnow")
+
+    def authenticate_user(self, user, password):
+        return self.user_db.get(user) == password
+
+
+class TestDatabase(object):
+    def __init__(self, host, port):
+        self.db = {}
+        self.user_db = {}
+
+    def authenticate_user(self, user, password):
+        return self.user_db.get(user, False) == password
+
+redis_database = RedisDatabase("localhost", 6379)
+test_dabase = TestDatabase()
+
+if __name__ == '__main__':
+    print database.authenticate_user("test", "test")
